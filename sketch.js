aa3809f2be741f93885e826e735f763f25a658a4
DhruvMaheshwari243 <76244578+DhruvMaheshwari243@users.noreply.github.com>
2021-07-14 17:04:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+var track, car1_img, car2_img, car3_img, car4_img;
+
+var carSound;
+
+var leaderboard,leaderboardImg;
+
+function preload(){
+  track = loadImage("../images/track.jpg");
+  car1_img = loadImage("../images/car1.png");
+  car2_img = loadImage("../images/car2.png");
+  car3_img = loadImage("../images/car3.png");
+  car4_img = loadImage("../images/car4.png");
+  ground = loadImage("../images/ground.png");
+  leaderboardImg = loadImage("images/leaderboard.png");
+  carSound = preload("car sound", "images/carsound.wav");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  leaderboard = createSprite(displayWidth/2, -(displayHeight * 4+200))
+  leaderboard.addImage(leaderboardImg);
+  leaderboard.scale = 0.7;
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+  if(cars < 200){
+    playSound(carSound, false)
+  }
+}
